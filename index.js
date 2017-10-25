d7ccef90ed720c0fbdc115fdc4d964e5b6eec43c
Péter Tóth <peter_toth1@epam.com>
2017-10-25 17:39:21 +0200

    init connection

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port = 3000;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/chatWindow.html');
 });
 
+io.on('connection', function(socket){
+    console.log('\n\na user connected', socket.handshake.query);
+});
+
 http.listen(port, function(){
-  console.log(`listening on *:${port}`);
+    console.log(`listening on *:${port}`);
 });
