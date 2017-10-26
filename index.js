934bf729133fac3b9349a55151dcdc256b43a9f8
Péter Tóth <peter_toth1@epam.com>
2017-10-26 12:19:19 +0200

    change index.js code style to ES6

--- index.js
+++ index.js
@@ -3,26 +3,17 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = 3000;
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/chatWindow.html');
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/chatWindow.html'));
 
-io.on('connection', function(socket){
+io.on('connection', socket =>
+{
     console.log('\na user connected', socket.handshake.query);
-
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
-
-    socket.on('chat message', function(msg){
-        console.log(socket.handshake.query.t,' message: ' + msg);
-    });
     
-    socket.on('disconnect', function(){
-        console.log('user disconnected',socket.handshake.query);
-    });
+    socket.on('chat message', msg => io.emit('chat message', msg));
+    
+    socket.on('chat message', msg => console.log(socket.handshake.query.t,' message: ' + msg));
+    
+    socket.on('disconnect', () => console.log('user disconnected',socket.handshake.query));
 });
 
-http.listen(port, function(){
-    console.log(`listening on *:${port}`);
-});
+http.listen(port, () => console.log(`listening on *:${port}`));
