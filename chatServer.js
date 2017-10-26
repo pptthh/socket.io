c6b156ab2459df1e708e6b4cff9fdcdd94a87f7d
Péter Tóth <peter_toth1@epam.com>
2017-10-26 13:33:34 +0200

    add server configuration object

--- chatServer.js
+++ chatServer.js
@@ -1,13 +1,23 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http,
+{
+    path: '/socket.io',
+    serveClient: true,
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    transports : ['websocket', 'polling'],
+    cookie: false,
+    //  adapter:socket.io-adapter  //  default value
+    //  parser:socket.io-parser
+});
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/chatWindow.html'));
 
 io.on('connection', socket =>
 {
-    console.log('\na user connected', socket.handshake.query);
+    console.log('a user connected', socket.handshake.query);
     
     socket.on('chat message', msg => io.emit('chat message', msg));
     
