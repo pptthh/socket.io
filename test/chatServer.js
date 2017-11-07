ff1f51db884b5c41b140f170fd42714f8ccd90e0
Peter Toth1 <peter_toth1@epam.com>
2017-11-07 18:20:44 +0100

    rename test file

--- test/chatServer.js
+++ test/chatServer.js
@@ -1,14 +1,15 @@
 'use strict';
 process.env.NODE_ENV = 'test';
-
 const assert = require('chai').assert;
 const request = require('supertest');
-const app = require('../app');
+const app = require('..' + __filename.split(__dirname)[1]);
 module.exports = app;
 
 
-describe('homepage', () => {
-    it('welcome the user',(done) => {
+describe('homepage', () =>
+{
+    it('welcome the user',(done) =>
+    {
         request(app).get('/')
             .expect(200)
             .expect('Content-Type', /html/)
