ab3793e8aedda3505758b3e8a0bd0def480db1d6
Péter Tóth <peter_toth1@epam.com>
2017-11-08 13:31:21 +0100

    have some simple running tests

--- test/chatServer.js
+++ test/chatServer.js
@@ -4,19 +4,48 @@ process.env.NODE_ENV = 'test';
 const assert = require('chai').assert;
 const request = require('supertest');
 
-//const app = require('..' + __filename.split(__dirname)[1]);
-//module.exports = app;
+const app = require('..' + __filename.split(__dirname)[1]);
  
 assert.isTrue(true);
 assert.isFalse(false);
-return;
+
+//return;
 describe('homepage', () =>
 {
-    it('welcome the user',(done) =>
+    it('check Socket.IO chat',(done) =>
     {
         request(app).get('/')
             .expect(200)
             .expect('Content-Type', /html/)
-            .expect(/Hello /, done)
+            .expect(/Socket.IO chat/, done)
+    });
+
+    it('check chat client JS',(done) => {
+        request(app).get('/p/chatClient.js')
+        .expect(200)
+        .expect('Content-Type', /javascript/)
+        .expect(/socket/)
+        .expect(/class Channel/)
+        .end(done)
+    });
+
+    it('check css',(done) => {
+        request(app).get('/p/chatWindow.css')
+        .expect(200)
+        .expect('Content-Type', /css/)
+        .expect(/body/)
+        .end(done)
+    });
+});
+
+
+describe('connect trough socket', () =>
+{
+    it('connect',(done) =>
+    {
+        request(app).get('/')
+            .expect(200)
+            .expect('Content-Type', /html/)
+            .expect(/Socket.IO chat/, done)
     })
 });
